990701a2164af2df99259e7dc81f334659639718
Jerry He <jerry_he@trendmicro.com>
2021-02-05 15:42:48 +0800

    add init files

---
+++ utils/const.py
@@ -0,0 +1,11 @@
+class CommonResult(object):
+    Success = 0
+    Fail = 1
+    Unknown = 999
+    Exist = need_to_provision = True
+    No_exist = no_need_to_provision = False
+
+
+class Status(object):
+    AVAILABLE = "available"
+    RUNNING = "running"
