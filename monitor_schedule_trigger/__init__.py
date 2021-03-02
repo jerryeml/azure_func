eb0edac7dd85bfeb0f12acd8339f1f966f895f46
Jerry He <jerry_he@trendmicro.com>
2021-03-02 22:02:18 +0800

    update monitor func

--- monitor_schedule_trigger/__init__.py
+++ monitor_schedule_trigger/__init__.py
@@ -7,14 +7,12 @@ import datetime
 import azure.functions as func
 from dotenv import load_dotenv, dotenv_values
 from subprocess import CalledProcessError
-from utils.helper import AzureDevopsAPI, AzureCLI, load_global_params_config
+from utils.helper import TaskAgent, Pipeline, AzureCLI, load_global_params_config
 
 
 class MonitorUtil(object):
     def __init__(self, circle_name):
         self.circle_name = circle_name
-        self.is_provision = 0
-        self.provision_release_id = load_global_params_config()['circle_var'][circle_name]['provision_release_id']
         self.user_name = load_global_params_config()['circle_var'][circle_name]['user_name']
         self.az_pat = os.getenv('AZ_PAT')
         self.sp_client_id = os.getenv('SP_CLIENT_ID')
@@ -23,27 +21,18 @@ class MonitorUtil(object):
         self.rg_dtl_name = load_global_params_config()['circle_var'][circle_name]['rg_dtl_name']
         self.minimun_available_count = load_global_params_config()['circle_var'][circle_name]['minimun_available_count']
         self.stage_list = load_global_params_config()['circle_var'][circle_name]['stage_list']
-        self.az_devops_api = AzureDevopsAPI(self.user_name, self.az_pat)
-        self.az_cli = AzureCLI(self.sp_client_id, self.sp_pwd, self.tenant_id)
-
-    def is_provision_machines(self):
-        self.monitor_vm_in_dtl()
-
-        if self.is_provision > 0:
-            return True
-        else:
-            return False
 
     def monitor_vm_in_dtl(self):
+        az_cli = AzureCLI(self.sp_client_id, self.sp_pwd, self.tenant_id)
         for stage in self.stage_list:
             try:
                 dtl_name = "dtl-" + self.circle_name.lower() + "-" + stage.lower()
-                result = self.az_cli.list_vm_in_dtl(dtl_name, self.rg_dtl_name)
+                result = az_cli.list_vm_in_dtl(dtl_name, self.rg_dtl_name)
                 available_vm_count = jmespath.search("length([?tags.status=='available'])", result)
 
-                if available_vm_count < self.minimun_available_count:
+                if available_vm_count <= self.minimun_available_count:
                     logging.info(f"Circle: {self.circle_name}, dtl name: {dtl_name}, available agent count: {available_vm_count} is less than minimun_count:{self.minimun_available_count}, do provision")
-                    self.is_provision += 1
+                    self.trigger_provision_job(stage)
                 else:
                     logging.info(f"Circle: {self.circle_name}, dtl name: {dtl_name}, available agent count: {available_vm_count}, no need provision")
             except CalledProcessError as e:
@@ -56,10 +45,11 @@ class MonitorUtil(object):
         """
         This func currently deprecated
         """
+        self.task_agent = TaskAgent(self.user_name, self.az_pat)
         self.dg_id_list = load_global_params_config()['circle_var'][self.circle_name]['dg_id_list']
 
         for dg_id in self.dg_id_list:
-            result = self.az_devops_api._get_deployment_group_agent(dg_id)
+            result = self.task_agent.get_deployment_group_agents(dg_id)
 
             available_agent_count = 0
             for each in result:
@@ -68,12 +58,37 @@ class MonitorUtil(object):
 
             if available_agent_count < self.minimun_available_count:
                 logging.info(f"Circle: {self.circle_name}, deployment group: {dg_id}, available agent count: {available_agent_count} is less than minimun_count:{self.minimun_available_count}, do provision")
-                self.is_provision += 1
             else:
                 logging.info(f"Circle: {self.circle_name}, deployment group: {dg_id}, available agent count: {available_agent_count}, no need provision")
 
-    def trigger_provision_job(self):
-        pass
+    def trigger_provision_job(self, stage):
+        """
+        trigger provision vm in specific dev test lab
+        """
+        provision_pipeline_id = load_global_params_config()['circle_var'][self.circle_name]['provision_pipeline_id']
+        pipeline = Pipeline(self.user_name, self.az_pat)
+
+        run_params = {
+            'variables': {
+                'app_name':
+                {
+                    'isSecret': False,
+                    'value': self.circle_name
+                },
+                'vm_count':
+                {
+                    'isSecret': False,
+                    'value': self.minimun_available_count
+                },
+                'env':
+                {
+                    'isSecret': False,
+                    'value': stage
+                }
+            }
+        }
+
+        pipeline.trigger_pipeline(run_params, provision_pipeline_id)
 
 
 def main(mytimer: func.TimerRequest) -> None:
@@ -89,20 +104,10 @@ def main(mytimer: func.TimerRequest) -> None:
     for circle in circle_list:
         logging.info(f"Prepare to monitor resource in {circle}")
         monitor_circle = MonitorUtil(circle)
-
-        if monitor_circle.is_provision_machines() is True:
-            pass
-            # result = monitor_circle.trigger_provision_job()
-            # assert result is True
+        monitor_circle.monitor_vm_in_dtl()
 
         logging.info(f"Circle: {circle}, Function complete")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
-    load_dotenv()
-    circle_list = list(load_global_params_config()['circle_var'].keys())
-    for circle in circle_list:
-        logging.info(f"Prepare to monitor resource in {circle}")
-        monitor_circle = MonitorUtil(circle)
-        print(monitor_circle.trigger_provision_job())
+    pass
